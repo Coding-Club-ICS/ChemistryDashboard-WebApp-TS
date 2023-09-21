d3e198b352d52bbc5ec54d981fa394ba6a8f4735
dongjin2008 <80329234+dongjin2008@users.noreply.github.com>
2023-09-21 07:31:29 +0200

    Dockerized backend

--- server/app.py
+++ server/app.py
@@ -1,168 +1,100 @@
 import os
-import psycopg2
-import psycopg2.pool
+import periodictable
 from chempy import balance_stoichiometry
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify, request, json
 from werkzeug.exceptions import HTTPException
+from sqlalchemy.schema import CreateSchema
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key'
+app.config.from_pyfile("config.py")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
-db_connection_pool = psycopg2.pool.SimpleConnectionPool(
-    minconn=1,
-    maxconn=5,
-    host=os.environ["DB_HOST"],
-    database=os.environ["DB_NAME"],
-    user=os.environ["DB_USER"],
-    password=os.environ["DB_PASSWORD"],
-    port=os.environ["DB_PORT"]
-)
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+DB_HOST = 'postgres'
+DB_PORT = '5432'
 
+DB_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 
-def create_db_connection():
-    try:
-        return db_connection_pool.getconn()
-    except psycopg2.Error as e:
-        app.logger.error("Error: Could not connect to database")
-        return None
+db = SQLAlchemy(app)
+
+valid_elements = [element.symbol for element in periodictable.elements]
 
 
-def create_tables():
-    connection = create_db_connection()
+class Element(db.Model):
+    __tablename__ = "elements"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
 
-    if connection is None:
-        return jsonify({"error": "Database connection failed"}), 500
-
-    try:
-        cursor = connection.cursor()
-
-        cursor.execute("CREATE TABLE IF NOT EXISTS elements.elements (id SERIAL PRIMARY KEY, name VARCHAR(50))")
-
-        connection.commit()
-        print("Tables created successfully")
-    except Exception as e:
-        print("Error: Could not create table")
-        print(e)
-    finally:
-        if connection:
-            db_connection_pool.putconn(connection)
+    def __init__(self, name):
+        self.name = name
 
 
-class ElementForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
     return "Hello, World!"
 
 
-@app.get("/chem/get_elements")
-def get_data():
-    connection = create_db_connection()
-
-    if connection is None:
-        return jsonify({"error": "Could not connect to database"}), 500
+@app.route("/chem/get_elements", methods=["GET"])
+def get_elements():
     try:
-        cursor = connection.cursor()
-        query = """
-SELECT * FROM elements.elements
-ORDER BY id ASC
-"""
-        cursor.execute(query)
-
-        data = cursor.fetchall()
-        return jsonify(data)
+        elements = Element.query.all()
+        element_list = [{"id": element.id, "name": element.name} for element in elements]
+        return jsonify({"elements": element_list})
     except Exception as e:
-        app.logger.error(f"Could not execute query: {e}")
-        return jsonify({"error": f"Could not execute query: {e}"})
-    finally:
-        if connection:
-            db_connection_pool.putconn(connection)
+        return jsonify({"error": f"Could not execute query: {e}"}), 500
 
 
-@app.post("/chem/add_element")
-def insert_element():
-    connection = create_db_connection()
-
-    if connection is None:
-        return jsonify({"error": "Database connection failed"}), 500
+@app.route("/chem/add_element", methods=["POST"])
+def add_element():
     try:
-        cursor = connection.cursor()
-
-        form = ElementForm()
-
-        data = request.json
-        name = data.get("name")
-
-
-        insert_query = "INSERT INTO elements.elements (name) VALUES (%s)"
-        cursor.execute(insert_query, (name,))
-
-        connection.commit()
-
+        name = request.json.get("name")
+        element = Element(name=name)
+        db.session.add(element)
+        db.session.commit()
         return jsonify({"message": "Element added successfully"})
     except Exception as e:
-        app.logger.error(f"Could not execute query: {e}")
-        return jsonify({"error": f"Could not execute query: {e}"})
-    finally:
-        if connection:
-            db_connection_pool.putconn(connection)
-
-
-@app.put("/chem/update_element/<int:id>")
-def update_element(id):
-    connection = create_db_connection()
-
-    if connection is None:
-        return jsonify({"error": "Database connection failed"}), 500
-    try:
-        cursor = connection.cursor()
-
-        data = request.json
-        name = data.get("name")
-
-        if name is None:
-            return jsonify({"error": "Name is required"}), 400
-
-        update_query = "UPDATE elements.elements SET name = %s WHERE id = %s"
-        cursor.execute(update_query, (name, id))
-
-        connection.commit()
-
-        return jsonify({"message": "Element updated successfully"})
-    except Exception as e:
-        return jsonify({"error": f"Could not execute query: {e}"})
-    finally:
-        if connection:
-            connection.close()
-
-
-@app.delete("/chem/delete_element/<int:id>")
-def delete_element(id):
-    connection = create_db_connection()
-
-    if connection is None:
-        return jsonify({"error": "Database connection failed"}), 500
-    try:
-        cursor = connection.cursor()
-
-        delete_query = "DELETE FROM elements.elements WHERE id = %s"
-        cursor.execute(delete_query, (id,))
-
-        connection.commit()
-
-        return jsonify({"message": "Element deleted successfully"})
-    except Exception as e:
-        app.logger.error(f"Could not execute query: {e}")
         return jsonify({"error": f"Could not execute query: {e}"}), 500
-    finally:
-        if connection:
-            db_connection_pool.putconn(connection)
 
-@app.get("/chem/balance/<string:equation>")
+with app.app_context():
+    db.create_all()
+
+@app.route("/chem/update_element/<int:id>", methods=["PUT"])
+def update_element(id):
+    try:
+        name = request.json.get("name")
+        element = Element.query.get(id)
+        if element:
+            element.name = name
+            db.session.commit()
+            return jsonify({"message": "Element updated successfully"})
+        else:
+            return jsonify({"error": "Element not found"}), 404
+    except Exception as e:
+        return jsonify({"error": f"Could not execute query: {e}"}), 500
+
+
+@app.route("/chem/delete_element/<int:id>", methods=["DELETE"])
+def delete_element(id):
+    try:
+        element = Element.query.get(id)
+        if element:
+            db.session.delete(element)
+            db.session.commit()
+            return jsonify({"message": "Element deleted successfully"})
+        else:
+            return jsonify({"error": "Element not found"}), 404
+    except Exception as e:
+        return jsonify({"error": f"Could not execute query: {e}"}), 500
+
+@app.route("/chem/balance/<string:equation>", methods=["GET"])
 def balance_equation(equation):
     try:
         equation = equation.split("=")
@@ -192,7 +124,17 @@ def balance_equation(equation):
 
         return jsonify({"result": result})
     except Exception as e:
-        print(e)
+        return jsonify({"error": f"Could not execute query: {e}"}), 500
+
+
+@app.route("/chem/periodictable", methods=["GET"])
+def get_periodic_table():
+    try:
+        elements = [element.symbol for element in periodictable.elements]
+        return jsonify({"elements": elements})
+    except Exception as e:
+        return jsonify({"error": f"Could not execute query: {e}"}), 500
+
 
 @app.errorhandler(HTTPException)
 def handle_exception(e):
@@ -207,5 +149,4 @@ def handle_exception(e):
 
 
 if __name__ == "__main__":
-    create_tables()
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ server/config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
